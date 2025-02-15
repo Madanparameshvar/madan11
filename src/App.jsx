bcdfd93e007b34bf87f6b8b6f793b56c62438115
Madanparameshvar <madanmadu146@gmail.com>
2025-02-15 16:40:13 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import react from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import react from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function App() {
-  const users = [
-    { id: 1, name: 'Alice',age:20 },
-    { id: 2, name: 'Bob',age:20 },
-    { id: 3, name: 'Charlie',age:20 },
-    { id: 4, name: 'Dave',  age:20 },
-  ];
-  const app=users.map((user) => (
-    <div key={user.id}>
-      <h2>{user.name}</h2>
-      <p>Age: {user.age}</p>
-    </div>
-  ));
-  return (
-    <>
-    {app}
-    </>
-  )
-}
+// function App() {
+//   const users = [
+//     { id: 1, name: 'Alice',age:20 },
+//     { id: 2, name: 'Bob',age:20 },
+//     { id: 3, name: 'Charlie',age:20 },
+//     { id: 4, name: 'Dave',  age:20 },
+//   ];
+//   const app=users.map((user) => (
+//     <div key={user.id}>
+//       <h2>{user.name}</h2>
+//       <p>Age: {user.age}</p>
+//     </div>
+//   ));
+//   return (
+//     <>
+//     {app}
+//     </>
+//   )
+// }
 
 
   
 
-export default App
+// export default App
